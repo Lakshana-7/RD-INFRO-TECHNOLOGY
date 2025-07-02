8da263241fddbc9cef165d95608c9c917c506496
Lakshana-7 <lakshanaa.240165@ritchennai.edu.in>
2025-07-02 16:46:54 +0530

    Added Task 2- Calculator App with README

---
+++ Task-2_Calculator/README.md
@@ -0,0 +1,20 @@
+# 🔢 Calculator App (Python)
+
+This is a simple command-line calculator built using Python.  
+It allows the user to perform basic arithmetic operations like:
+
+- Addition
+- Subtraction
+- Multiplication
+- Division (with zero handling)
+
+---
+
+## 💻 How to Run
+
+```bash
+python calculator.py
+
+## 📹 Demo
+
+🎥 [Click here to watch the demo](  )
---
+++ Task-2_Calculator/calculator.py
@@ -0,0 +1,48 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Cannot divide by zero!"
+    return x / y
+
+def show_menu():
+    print("\n===== Simple Calculator =====")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Exit")
+
+while True:
+    show_menu()
+    choice = input("Enter your choice (1–5): ")
+
+    if choice in ['1', '2', '3', '4']:
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+        except ValueError:
+            print("Please enter valid numbers.")
+            continue
+
+        if choice == '1':
+            print("Result:", add(num1, num2))
+        elif choice == '2':
+            print("Result:", subtract(num1, num2))
+        elif choice == '3':
+            print("Result:", multiply(num1, num2))
+        elif choice == '4':
+            print("Result:", divide(num1, num2))
+
+    elif choice == '5':
+        print("Thank you for using the calculator. Goodbye!")
+        break
+    else:
+        print("Invalid input. Please choose from 1 to 5.")
